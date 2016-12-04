e6f36c1ddc51cad927e540a579594482c699d5c6
Tom Hallam <tom.hallam40@googlemail.com>
2016-12-04 22:21:50 +0000

    bufferstream in client stopping audio cut-outs

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 const net = require('net');
 const Speaker = require('speaker');
-const through2 = require('through2');
+const filter = require('through2-filter');
+const BufferStream = require('bufferstream');
 
 // Create the Speaker instance
 var speakerInstance = new Speaker({
@@ -13,10 +14,12 @@ var HOST = '127.0.0.1';
 var PORT = 3001;
 
 var client = new net.Socket();
+var pcmBuffer = new BufferStream();
 client.connect(PORT, HOST, function() {
 
     console.log('CONNECTED TO: ' + HOST + ':' + PORT);
     client
+      .pipe(pcmBuffer)
       .pipe(speakerInstance);
 
 });
